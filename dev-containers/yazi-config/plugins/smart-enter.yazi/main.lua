1bd0139bd6dde0d7549b6ba38a591c709c50d99f
Lario <lario@example.com>
2026-06-14 07:16:42 +0200

    Update Yazi configuration: Fix [mgr] keymap header and migration to ya.emit

--- dev-containers/yazi-config/plugins/smart-enter.yazi/main.lua
+++ dev-containers/yazi-config/plugins/smart-enter.yazi/main.lua
@@ -1,15 +1,14 @@
-local is_dir = ya.sync(function()
-      local h = cx.active.current.hovered
-      return h and h.cha.is_dir or false
-end)
-
+--- @sync entry
 return {
-      entry = function()
-              if is_dir() then
-                      ya.mgr_emit("enter", {})
-                      ya.mgr_emit("quit", {})
-              else
-                      ya.mgr_emit("open", { hovered = true })   -- open the hovered file
-              end
-      end,
+	entry = function()
+		local h = cx.active.current.hovered
+		local is_dir = h and h.cha.is_dir or false
+
+		if is_dir then
+			ya.emit("enter", {})
+			ya.emit("quit", {})
+		else
+			ya.emit("open", { hovered = true })
+		end
+	end,
 }
